d75f72722c83dfb8281f35e03493f0f64c56d312
Impero <1209058433@qq.com>
2022-11-17 16:36:56 +0800

    up

--- connection/db.ts
+++ connection/db.ts
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 
 //链接数据库
 const dbArticle: any = mongoose.createConnection(
-    // 'mongodb://localhost:27017/blog',
-    'mongodb://root:1209058433@123.56.102.179:27017/?directConnection=true&authMechanism=DEFAULT&authSource=admin',
+    'mongodb://localhost:27017/blog',
     err => {
         if (err) {
             return console.log('文章数据库连接失败' + err);
