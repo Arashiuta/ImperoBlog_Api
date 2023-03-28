7ff3275cfb673a72649310dabb3718a068a0b6db
yi-blow <96853279+1209058433@users.noreply.github.com>
2023-03-28 17:39:10 +0800

    Update db.js

--- connection/db.js
+++ connection/db.js
@@ -3,11 +3,7 @@ import mongoose from "mongoose";
 
 //链接数据库
 const dbArticle = mongoose.createConnection(
-    'mongodb://127.0.0.1:27017/blog',
-<<<<<<< HEAD
-    // 'mongodb://root:admin123@101.43.176.92:27017/?directConnection=true&authMechanism=DEFAULT&authSource=admin',
-=======
->>>>>>> 5ebb24f734ef352a7f908ef39d75c436db5a1e96
+    'mongodb://127.0.0.1:27017/blog,
     err => {
         if (err) {
             return console.log('文章数据库连接失败' + err);
